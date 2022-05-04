815760073ded4332af69c428572044a7c8a46b87
ihappysoni <96763614+ihappysoni@users.noreply.github.com>
2022-05-04 16:06:39 +0530

    intro

--- src/App.js
+++ src/App.js
@@ -4,20 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>About Myself</h1>
+      <p>
+        <ul>
+          <li>My Name is <b> Happy Soni</b></li>
+          <li>I,ve done my bachelors in Computer Application and its result is announced a week ago in which i got 9.75 SGPA in final sem forming an average of 9.37 SGPA</li>
+          <li>I,m skilled in HTML,CSS,JavaScrit,PHP and currently learning React Library for front end . </li>
+          <li>I am currently enrolled in Full Stack Web Development Course from PrepBytes Elevation Academy.</li>
+          <li>i have some experience in Java and C++ Development with MySQL </li>
+          <li>I have sound knowledge of Operating Sytem, Networking & Cloud Computing</li>
+          <li>I am fond of learning new technologies ,apart from full stack web development , i am planning to learn basics of DevOps in upcoming days also i have some interest in sports like cricket and badminton.</li>
+          <li>Thank You , That's All from my side.</li>
+        </ul>
+      </p>
     </div>
   );
 }
